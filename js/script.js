08bbf2d5a311d7c08c7d0a2f15d595d8210958f0
domenicog03 <domenico.galeone.info@gmail.com>
2024-03-05 16:35:30 +0100

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ const colore= prompt('Digita il tuo colore preferito');
 const pass=nome+cognome+colore+'23';
 
 document.getElementById('pass').innerHTML = pass;
+
+
+const num=parseInt(prompt('Digita un numero'));
+
+const pass2=nome+cognome+colore+(23+num);
+//console.log(pass2);
+
+document.getElementById('pass2').innerHTML = pass2;
